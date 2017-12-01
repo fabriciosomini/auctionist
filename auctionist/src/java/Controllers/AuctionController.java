22f75154d1ae2e1a00b93a81f90deb1b3e71587e
Somini <fabricio_somini.lumia@live.com>
2017-11-30 22:57:57 -0200

    3011-2130

--- auctionist/src/java/Controllers/AuctionController.java
+++ auctionist/src/java/Controllers/AuctionController.java
@@ -8,6 +8,7 @@ package Controllers;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author fabri
  */
+@WebServlet(urlPatterns = {"/auctionist"})
 public class AuctionController extends HttpServlet {
 
     /**
