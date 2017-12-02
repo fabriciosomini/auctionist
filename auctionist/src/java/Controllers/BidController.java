9057ad175803339212940ec9a3a7975a173def5f
Somini <fabricio_somini.lumia@live.com>
2017-12-01 23:00:18 -0200

    login

--- auctionist/src/java/Controllers/BidController.java
+++ auctionist/src/java/Controllers/BidController.java
@@ -21,32 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/save-bid", "/update-bid", "/delete-bid"})
 public class BidController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BidController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BidController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -59,7 +34,7 @@ public class BidController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
     }
 
     /**
@@ -73,7 +48,7 @@ public class BidController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /**
